6aa2709b926578684bf82b141f01ca8b7d93a101
Teerapat <teerapatkij1412@gmail.com>
2019-10-01 18:46:22 +0700

    add send data

--- app/src/main/kotlin/scb/academy/jinglebell/fragment/ProfileFragment.kt
+++ app/src/main/kotlin/scb/academy/jinglebell/fragment/ProfileFragment.kt
@@ -1,14 +1,30 @@
 package scb.academy.jinglebell.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 import scb.academy.jinglebell.R
+import scb.academy.jinglebell.activity.HelloActivity
 
 class ProfileFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setView()
+    }
+
+    private fun setView() {
+        btnAdd.setOnClickListener {
+            var name = inputName.text.toString()
+
+            context?.let { it1 -> HelloActivity.startActivity(it1, name) }
+        }
     }
 }
